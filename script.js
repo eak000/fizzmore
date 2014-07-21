002eae940646848adf30acd068da7e499d18576e
Emily Kelton <emilyk000@gmail.com>
2014-07-20 19:45:04 -0700

    fizzbuzz function working but not validation

--- script.js
+++ script.js
@@ -2,38 +2,67 @@ window.onload = function() {
 
 var numValue = parseInt(prompt("Let's play FizzBuzz. \nEnter a number between 1 and 100"));
 
-var getNum = function() {
+// alert(typeof(numValue));
 
-	numValue = parseInt(prompt("please enter a whole number between 1 and 100"));
 
-	// numValue = parseInt(numValue);	
+function getNum() {
+
+// numValue = parseInt(prompt("please enter a whole number between 1 and 100"));
 
 		if (isNaN(numValue)) {
 			getNum();
 		} else {
-			fizzBuzz(numValue);
+			fizzBuzz();
 		};
+		
+	}; //getNum end
+	
+// var test = 99;
 
-}; //getNum end
+function fizzBuzz () {
 
-var fizzBuzz = function() {
+
 			for (var i = 1; i < numValue + 1; i++) {
 			if (i % 3 === 0 && i % 5 === 0) {
-				document.getElementById('container').innerHTML = ("fizzbuzz")
+				// console.log("fizzbuzz");
+				
+				var para = document.createElement("p"); //creates a <p> tag
+				var node = document.createTextNode("fizzbuzz"); //creates node to add text to
+				para.appendChild(node); //add node to p element
+				var div = document.getElementById("container"); 
+				div.appendChild(para); //adds <p> to div
 			} 
 			else if (i % 3 === 0) {
-				document.getElementById('container').innerHTML = ("fizz")
+				// console.log("fizz");
+				
+				var para = document.createElement("p"); //creates a <p> tag
+				var node = document.createTextNode("fizz");
+				para.appendChild(node);
+				var div = document.getElementById("container");
+				div.appendChild(para);
 				} 
 
 			else if (i % 5 === 0) {
-					document.getElementById('container').innerHTML = ("buzz")
+				// console.log('buzz');
+				
+				var para = document.createElement("p"); //creates a <p> tag
+				var node = document.createTextNode("buzz");
+				para.appendChild(node);
+				var div = document.getElementById("container");
+				div.appendChild(para);
 				} else {
-					document.getElementById('container').innerHTML = (i)
+					// console.log(i);
+				
+				var para = document.createElement("p"); //creates a <p> tag	
+				var node = document.createTextNode(i);
+				para.appendChild(node);
+				var div = document.getElementById("container");
+				div.appendChild(para);
 				};
 			}; //for loop end
+
+
 }; //fizzBuzz end
-
 fizzBuzz();
-
 
 }; //onload end
